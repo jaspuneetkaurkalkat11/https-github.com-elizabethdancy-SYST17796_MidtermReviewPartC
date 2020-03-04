5040be6c596ab777dd1d5ac13ef643ceed408ef8
bansaani <59941822+bansaani@users.noreply.github.com>
2020-03-04 18:16:06 -0500

    Update User.java

--- src/midtermreviewcodeforpartc/User.java
+++ src/midtermreviewcodeforpartc/User.java
@@ -3,6 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+// i am studying in sheridan college.
+
 package midtermreviewcodeforpartc;
 
 /**
