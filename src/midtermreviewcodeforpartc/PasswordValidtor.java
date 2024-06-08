9f60c7efebb5072eace525bb82ed9434782ea534
jaspuneetkaurkalkat11 <kalkjasp@sheridancollege.ca>
2024-06-07 21:28:41 -0400

    Update PasswordValidtor.java

--- src/midtermreviewcodeforpartc/PasswordValidtor.java
+++ src/midtermreviewcodeforpartc/PasswordValidtor.java
@@ -1,1 +1,23 @@
+public class PasswordValidator {
+    /**
+     * Validates the password based on specified rules.
+     * @param password The password to validate.
+     * @return true if the password is valid, false otherwise.
+     */
+    public boolean isValid(String password) {
+        return isLengthValid(password) && hasSpecialCharacter(password);
+    }
 
+    private boolean isLengthValid(String password) {
+        return password.length() >= 8;
+    }
+
+    private boolean hasSpecialCharacter(String password) {
+        for (char c : password.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
